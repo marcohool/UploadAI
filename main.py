23ee1c8992f37c35453b03447a174cbcf6b8d17b
Marco <marco.boutchelski@gmail.com>
2023-12-17 01:22:29 +0000

    Update dalle prompt to json response

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import schedule
 import random
 import time
 from geopy.geocoders import Nominatim
+import json
 
 
 dotenv_path = Path('.env')
@@ -85,13 +86,15 @@ def main():
     randomCountry = getCountry("data/countryList.txt")
     print("Got country -> ", randomCountry)
 
-    dallePrompt = getTextResponse(client, 1, f"Design a prompt for DALL-E 3 to produce an attention-grabbing realistic image of the beauty of {randomCountry}. Pick a place in {randomCountry} with a name and base your image there. Please include specific details such as the desired setting, objects, colors, mood, and unique elements to be incorporated. Additionally, consider the mood and atmosphere you want to convey, and provide descriptive adjectives to guide the image creation. Specify the desired perspective and composition, as well as the preferred lighting and time of day. If applicable, indicate any desired action or movement within the image. Aim for a balance between providing sufficient detail and conciseness in your prompt. Feel free to employ analogies or comparisons to further clarify your vision. Lastly, indicate any desired styles or themes, and outline an iterative approach for refining the image. Return nothing but this prompt, no other text. The first word of your response should contain just the name of the place you are drawing as a location (town, city) within {randomCountry}, nothing else.")
+    dallePrompt = getTextResponse(client, 1, f"Design a prompt for DALL-E 3 to produce an attention-grabbing realistic image of the beauty of {randomCountry}. Pick a place in {randomCountry} with a name and base your image there. Please include specific details such as the desired setting, objects, colors, mood, and unique elements to be incorporated. Additionally, consider the mood and atmosphere you want to convey, and provide descriptive adjectives to guide the image creation. Specify the desired perspective and composition, as well as the preferred lighting and time of day. If applicable, indicate any desired action or movement within the image. Aim for a balance between providing sufficient detail and conciseness in your prompt. Feel free to employ analogies or comparisons to further clarify your vision. Lastly, indicate any desired styles or themes, and outline an iterative approach for refining the image. Return nothing but this prompt, no other text. The first word of your response should contain just the name of the place you are drawing as a location (town, city) within {randomCountry}, nothing else. Respond in a json format, with the first field 'chosenLocation' and second 'promptValue'")
 
-    # The first sentence is the first element of the 'sentences' list
-    locationGenerted = dallePrompt.split(" ")[0]
-    print("Location generated -> ", locationGenerted)
+    # Parse JSON response
+    jsonPrompt = json.loads(dallePrompt)
 
-    # The rest of the content is the remaining sentences joined back together
+    locationGenerted = jsonPrompt["chosenLocation"]
+    dallePrompt = jsonPrompt["promptValue"]
+
+    print("\nDalle location generated -> ", locationGenerted)
     print("\nDalle prompt generated -> ", dallePrompt)
 
     # Generate image
@@ -124,3 +127,4 @@ if __name__ == "__main__":
     while True:
         schedule.run_pending()
         time.sleep(1)
+
