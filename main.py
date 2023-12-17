ce1902bcf71c7e946534125a0266671c72b0e8bb
Marco <marco.boutchelski@gmail.com>
2023-12-17 10:37:02 +0000

    Add model option to getTextResponse

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ client = OpenAI(
 )
 
 
-def getTextResponse(client, temperature, inputText):
+def getTextResponse(client, model, temperature, inputText):
     chat_completion = client.chat.completions.create(
-        model="gpt-4",
+        model=model,
         temperature=temperature,
         messages=[
             {
@@ -86,7 +86,7 @@ def main():
     randomCountry = getCountry("data/countryList.txt")
     print("Got country -> ", randomCountry)
 
-    dallePrompt = getTextResponse(client, 1, f"Design a prompt for DALL-E 3 to produce an attention-grabbing realistic image of the beauty of {randomCountry}. Pick a place in {randomCountry} with a name and base your image there. Please include specific details such as the desired setting, objects, colors, mood, and unique elements to be incorporated. Additionally, consider the mood and atmosphere you want to convey, and provide descriptive adjectives to guide the image creation. Specify the desired perspective and composition, as well as the preferred lighting and time of day. If applicable, indicate any desired action or movement within the image. Aim for a balance between providing sufficient detail and conciseness in your prompt. Feel free to employ analogies or comparisons to further clarify your vision. Lastly, indicate any desired styles or themes, and outline an iterative approach for refining the image. Return nothing but this prompt, no other text. The first word of your response should contain just the name of the place you are drawing as a location (town, city) within {randomCountry}, nothing else. Respond in a json format, with the first field 'chosenLocation' and second 'promptValue'")
+    dallePrompt = getTextResponse(client, "gpt-3.5-turbo", 1, f"Design a prompt for DALL-E 3 to produce an attention-grabbing realistic image of the beauty of {randomCountry}. Pick a place in {randomCountry} with a name and base your image there. Please include specific details such as the desired setting, objects, colors, mood, and unique elements to be incorporated. Additionally, consider the mood and atmosphere you want to convey, and provide descriptive adjectives to guide the image creation. Specify the desired perspective and composition, as well as the preferred lighting and time of day. If applicable, indicate any desired action or movement within the image. Aim for a balance between providing sufficient detail and conciseness in your prompt. Feel free to employ analogies or comparisons to further clarify your vision. Lastly, indicate any desired styles or themes, and outline an iterative approach for refining the image. Return nothing but this prompt, no other text. The first word of your response should contain just the name of the place you are drawing as a location (town, city) within {randomCountry}, nothing else. Respond in a json format, with the first field 'chosenLocation' and second 'promptValue'")
 
     # Parse JSON response
     jsonPrompt = json.loads(dallePrompt)
@@ -106,7 +106,7 @@ def main():
 
     # Get photo caption
     caption = getTextResponse(
-        client, 1, f"Generate a 1 or 2 sentence caption for this image description, describing it briefly, along with a list of total 10-15 hashtags for social media use. Please reply with this caption and nothing else: '{dallePrompt}'")
+        client, "gpt-3.5-turbo", 1, f"Generate a 1 or 2 sentence caption for this image description, describing it briefly, along with a list of total 10-15 hashtags for social media use. Please reply with this caption and nothing else: '{dallePrompt}'")
 
     # Add space between hashtags and caption
     hash_index = caption.find('#')
@@ -127,4 +127,3 @@ if __name__ == "__main__":
     while True:
         schedule.run_pending()
         time.sleep(1)
-
