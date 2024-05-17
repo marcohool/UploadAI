d37a9df28d5e0bea0c14f1a35af4b55d1854e5b0
Marco <marco.boutchelski@gmail.com>
2024-05-17 23:18:39 +0100

    Re-enable upload schedule

--- src/main.py
+++ src/main.py
@@ -17,64 +17,63 @@ prompts = load_prompts()
 
 
 def main():
-    # try:
-    # Get random country to generate image of
-    randomCountry = get_random_country("data/countryList.txt")
-    print("Got country -> ", randomCountry)
+    try:
+        # Get random country to generate image of
+        randomCountry = get_random_country("data/countryList.txt")
+        print("Got country -> ", randomCountry)
 
-    # Get random time of day for image to be generated in
-    timeOfDay = get_random_time_of_day()
-    print("Got time of day -> ", timeOfDay)
+        # Get random time of day for image to be generated in
+        timeOfDay = get_random_time_of_day()
+        print("Got time of day -> ", timeOfDay)
 
-    # Get prompt to use for image generation
-    dalle_prompt_json = openAi.get_text_response(1.2, prompts['dalle_prompt_template'].format(
-        randomCountry=randomCountry, timeOfDay=timeOfDay))
+        # Get prompt to use for image generation
+        dalle_prompt_json = openAi.get_text_response(1.2, prompts['dalle_prompt_template'].format(
+            randomCountry=randomCountry, timeOfDay=timeOfDay))
 
-    print("Got prompt -> ", dalle_prompt_json)
-    chosen_location, dalle_prompt = process_dalle_prompt_request(
-        dalle_prompt_json)
+        print("Got prompt -> ", dalle_prompt_json)
+        chosen_location, dalle_prompt = process_dalle_prompt_request(
+            dalle_prompt_json)
 
-    # Generate image
-    caption = handle_image_generation(openAi,
-                                      dalle_prompt, "assets/dalleImage.jpg", True)
+        # Generate image
+        caption = handle_image_generation(openAi,
+                                          dalle_prompt, "assets/dalleImage.jpg", True)
 
-    # Upload photo
-    upload_photo("assets/dalleImage.jpg", caption,
-                 f"{chosen_location}, {randomCountry}")
+        # Upload photo
+        upload_photo("assets/dalleImage.jpg", caption,
+                     f"{chosen_location}, {randomCountry}")
 
-    # except Exception as e:
-    #     print("Couldn't perform main: ", e)
+    except Exception as e:
+        print("Couldn't perform main: ", e)
 
 
 if __name__ == "__main__":
-    main()
-    # # Monday
-    # schedule.every().monday.at("09:00").do(main)
-    # schedule.every().monday.at("17:00").do(main)
+    # Monday
+    schedule.every().monday.at("09:00").do(main)
+    schedule.every().monday.at("17:00").do(main)
 
-    # # Tuesday
-    # schedule.every().tuesday.at("09:00").do(main)
-    # schedule.every().tuesday.at("17:00").do(main)
+    # Tuesday
+    schedule.every().tuesday.at("09:00").do(main)
+    schedule.every().tuesday.at("17:00").do(main)
 
-    # # Wednesday
-    # schedule.every().wednesday.at("05:00").do(main)
-    # schedule.every().wednesday.at("17:00").do(main)
+    # Wednesday
+    schedule.every().wednesday.at("05:00").do(main)
+    schedule.every().wednesday.at("17:00").do(main)
 
-    # # Thursday
-    # schedule.every().thursday.at("12:00").do(main)
-    # schedule.every().thursday.at("18:00").do(main)
+    # Thursday
+    schedule.every().thursday.at("12:00").do(main)
+    schedule.every().thursday.at("18:00").do(main)
 
-    # # Friday
-    # schedule.every().friday.at("09:00").do(main)
-    # schedule.every().friday.at("17:00").do(main)
+    # Friday
+    schedule.every().friday.at("09:00").do(main)
+    schedule.every().friday.at("17:00").do(main)
 
-    # # Saturday
-    # schedule.every().saturday.at("10:00").do(main)
+    # Saturday
+    schedule.every().saturday.at("10:00").do(main)
 
-    # # Sunday
-    # schedule.every().sunday.at("09:00").do(main)
-    # schedule.every().sunday.at("18:00").do(main)
+    # Sunday
+    schedule.every().sunday.at("09:00").do(main)
+    schedule.every().sunday.at("18:00").do(main)
 
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
